05a57844b2c3cc159be3dcb8ffc70a0e2f2aa004
Saumya Kesur <saumyakesur@gmail.com>
2026-05-18 13:36:06 +0530

    Add files via upload

---
+++ React/FirstProject/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import Testapp from './Testapp.jsx'
+// import Secondapp from './Secondapp.jsx'
+import Rules from './Rules.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <Testapp />
+    <Secondapp/> */}
+    {/* <App /> */}
+    <Rules />
+  </StrictMode>
+)
