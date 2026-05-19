c3249006e383bd443684948d07835344b1b49608
Saumya Kesur <saumyakesur@gmail.com>
2026-05-19 21:39:15 +0530

    Updated Rules.jsx And main.jsx

--- React/ComponenetsAndProps/src/Rules.jsx
+++ React/ComponenetsAndProps/src/Rules.jsx
@@ -1,12 +1,13 @@
+let num = 0
+
 function Rules() {
     let name = "Saumya"
-    let num = 0;
     return (
         <>
             <h1>Welcome {name}</h1>
             <p>Calculation of 10 + 10 = {10+10}</p>
-            <button onClick={()=>document.querySelector(".p1").textContent = num+1}>Increment</button>
-            <p className="p1"></p>
+            <button onClick={() => { num = num + 1; document.querySelector('.p1').textContent = num; }}>Increment</button>
+            <p className="p1">0</p>
         </>
     )
 }
--- React/ComponenetsAndProps/src/main.jsx
+++ React/ComponenetsAndProps/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-// import Testapp from './Testapp.jsx'
-// import Secondapp from './Secondapp.jsx'
+import Testapp from './Testapp.jsx'
+import Secondapp from './Secondapp.jsx'
 import Rules from './Rules.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Testapp />
-    <Secondapp/> */}
+    <Testapp />
+    <Secondapp/>
     {/* <App /> */}
     <Rules />
   </StrictMode>
