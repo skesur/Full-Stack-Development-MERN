3f240c57eb6467e363373b0cd6324b9190b333f1
Saumya Kesur <saumyakesur@gmail.com>
2026-05-19 19:35:23 +0530

    Added ComponentsAndProps in React

---
+++ React/ComponenetsAndProps/src/Secondapp.jsx
@@ -0,0 +1,13 @@
+function Secondapp() {
+    {/*
+        ONLY RETURN ONE ELEMENT
+        */}
+    return (
+        <>
+            <h1>Secondapp Data</h1>
+            <p>DATA11... DATA22... DATA33...</p>
+        </>
+    )
+}
+
+export default Secondapp
