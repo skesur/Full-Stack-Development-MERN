f8b6160ff21647766e2a98aa57e5c90da73c17c5
Saumya Kesur <saumyakesur@gmail.com>
2026-05-30 13:25:12 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Todo from './Todo'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Todo />
-  </StrictMode>
-)
